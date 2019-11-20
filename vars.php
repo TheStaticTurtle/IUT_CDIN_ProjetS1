db902d237bb0f44f6fefd2beff247d66a6cbec51
Lucas <starlucas68@laposte.net>
2019-11-20 08:50:54 +0100

    Create vars.php

---
+++ vars.php
@@ -0,0 +1,17 @@
+$players = array(
+	1 => array(  "nom" => "Lloris", "prenom" => "Hugo", "poste" => "Gardien"),
+	2 => array(  "nom" => "Pavard", "prenom" => "Benjamin", "poste" => "Défenseur"),
+	4 => array(  "nom" => "Varane", "prenom" => "Raphaël", "poste" => "Défenseur"),
+	5 => array(  "nom" => "Lenglet", "prenom" => "Clément", "poste" => "Défenseur"),
+	6 => array(  "nom" => "Ndombele", "prenom" => "Tanguy", "poste" => "Milieu"),
+	7 => array(  "nom" => "Griezmann", "prenom" => "Antoine", "poste" => "Attaquant"),
+	9 => array(  "nom" => "Giroud", "prenom" => "Olivier", "poste" => "Attaquant"),
+	10 => array(  "nom" => "Mbappé", "prenom" => "Kylian", "poste" => "Attaquant"),
+	12 => array(  "nom" => "Tolisso", "prenom" => "Corentin", "poste" => "Milieu"),
+	13 => array(  "nom" => "Kanté", "prenom" => "N'Golo", "poste" => "Milieu"),
+	16 => array(  "nom" => "Mandanda", "prenom" => "Steve", "poste" => "Gardien"),
+	17 => array(  "nom" => "Sissoko", "prenom" => "Moussa", "poste" => "Milieu"),
+	18 => array(  "nom" => "Fékir", "prenom" => "Nabil", "poste" => "Attaquant"),
+	21 => array(  "nom" => "Dubois", "prenom" => "Léo", "poste" => "Défenseur"),
+	23 => array(  "nom" => "Areola", "prenom" => "Alphonse", "poste" => "Gardien"),
+);
