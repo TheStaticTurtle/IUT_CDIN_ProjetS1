deb58138ac9cb5c462ef5fd31678c60a2befd388
TurtleForGaming <tuglersamuel@gmail.com>
2019-11-20 16:23:11 +0100

    Update

--- template.php
+++ template.php
@@ -24,14 +24,13 @@ if(isset($_POST["player"])) {
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<style type="text/css">
-		body {font-size:3rem;} /*1rem = 16px*/
-		h1 {font-size:5.5rem;} /*1rem = 16px*/
+		body {font-size:3rem;}
+		h1 {font-size:5.5rem;}
 		button {font-size: 3rem !important }
 		a {font-size: 3rem !important;}
 		.linkNav {font-size: 2rem !important}
-		/*span {font-size:3rem;} /*1rem = 16px*/
 
-		/* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+		/* Medium devices (tablets, 768px and up)*/
 		@media (min-width: 600px) {  
 			body {font-size:1rem;} /*1rem = 16px*/
 			h1 {font-size:3rem;} /*1rem = 16px*/
@@ -119,7 +118,7 @@ if(isset($_POST["player"])) {
 		</div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
