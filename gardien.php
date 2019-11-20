74f3f11be4775f8ccc4a65d41b4e37cec550ea98
Lucas <starlucas68@laposte.net>
2019-11-20 10:39:04 +0100

    Update gardien.php
    Correction des fautes d'orthographe

--- gardien.php
+++ gardien.php
@@ -1,7 +1,7 @@
 <?php
 
 include("vars.php"); 
-$position = "gardien";
+$position = "Gardien";
 
 if(isset($_POST["player"])) {
 	$id = $_POST["player"];
@@ -20,21 +20,21 @@ if(isset($_POST["player"])) {
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="index.php">Getion équipe</a>
+		<a class="navbar-brand" href="index.php">Gestion équipe</a>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="#">Gardient</a>
+					<a class="nav-link" href="gardien.php">Gardien</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Defenseur</a>
+					<a class="nav-link" href="defenseur.php">Defenseur</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Milieu</a>
+					<a class="nav-link" href="milieu.php">Milieu</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Attackant</a>
+					<a class="nav-link" href="attaquant.php">Attaquant</a>
 				</li>
 			</ul>
 		</div>
@@ -43,7 +43,7 @@ if(isset($_POST["player"])) {
 	<div class="container" style="margin-top: 1%">
 		<div class="row">
 			<div class="col">
-				<button type="button" class="btn btn-danger">Remise a zero</button>
+				<button type="button" class="btn btn-danger">RAZ</button>
 				<ul style="margin-top: 3%">
 					<li> <b>Equipe actuelle</b> </li>
 
@@ -57,9 +57,9 @@ if(isset($_POST["player"])) {
 				<table style="margin-top: 3%" class="table">
 					<thead>
 						<tr>
-							<th scope="col">Prenom</th>
+							<th scope="col">Prénom</th>
 							<th scope="col">Nom</th>
-							<th scope="col">Numero</th>
+							<th scope="col">Numéro</th>
 							<th scope="col">Action</th>
 						</tr>
 					</thead>
